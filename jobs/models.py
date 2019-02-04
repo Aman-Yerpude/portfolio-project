cf8fca6dbea133f0e956f6a84a677e2f0f7be839
Aman-Yerpude <yerpudeaman@gmail.com>
2019-02-04 17:17:01 +0530

    Added Bootstrap and Jobs are Showing Up

--- jobs/models.py
+++ jobs/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=32)
     summary = models.CharField(max_length=200)
